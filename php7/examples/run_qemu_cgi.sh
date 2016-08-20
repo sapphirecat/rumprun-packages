c87cdb9520c45aa8925a7e70eddb2a064a90345e
Sapphire Cat <devel@sapphirepaw.org>
2016-08-20 13:19:02 -0400

    Tune qemu memory size.

--- php7/examples/run_qemu_cgi.sh
+++ php7/examples/run_qemu_cgi.sh
@@ -3,7 +3,9 @@ set -x
 GUEST_PORT="${GUEST_PORT:-${1-9000}}"
 HOST_PORT="${HOST_PORT:-${2-8080}}"
 HYPERVISOR="${HYPERVISOR:-qemu}"
-rumprun ${1+"$@"} "$HYPERVISOR" -i -M 512 \
+# php has a heap limit of 128M by default, provide some code/stack space on top of that
+MEM_LIMIT="${MEM_LIMIT:-192}"
+rumprun ${1+"$@"} "$HYPERVISOR" -i -M "$MEM_LIMIT" \
 	-I 'qnet0,vioif,-net user' \
 	-g "-redir tcp:${HOST_PORT}::${GUEST_PORT}" \
 	-W qnet0,inet,dhcp \
